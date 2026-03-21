87d38621fc676e280dd4125182d46650c39efd9e
Pablo Fernandez <pablo.fernandezcornejo@gmail.com>
2026-03-21 21:56:24 +0100

    Js tienda terminado

--- js/tienda_script.js
+++ js/tienda_script.js
@@ -9,8 +9,26 @@ for(let i = 0; i<botones.length;i++){
 
 function cuentas(){
     if(this.classList.contains('btn-monedas')){
+        let precioMon = parseInt(this.textContent);
+        let totalMonedas = parseInt(cantidadMonedas.textContent);
 
+        if(precioMon <= totalMonedas){
+            totalMonedas -= precioMon;
+            cantidadMonedas.textContent = totalMonedas;
+            alert("Compra Realizada");
+        }else{
+            alert("Saldo insuficiente");
+        }
     }else if(this.classList.contains('btn-gemas')){
+        let precioGem = parseInt(this.textContent);
+        let totalGemas = parseInt(cantidadGemas.textContent);
 
+        if(precioGem <= totalGemas){
+            totalGemas -= precioGem;
+            cantidadGemas.textContent = totalGemas;
+            alert("Compra Realizada");
+        }else{
+            alert("Saldo insuficiente");
+        }
     }
 }

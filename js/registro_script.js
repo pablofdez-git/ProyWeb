8753d705251c69c9de1f5af4ced5cdb5f2365c3e
Sys-Mario <mariog.iglesias@gmail.com>
2026-04-01 16:16:16 +0200

    JS en registro y estadisticas con color

--- js/registro_script.js
+++ js/registro_script.js
@@ -1,17 +1,32 @@
-let boxDisable = document.getElementByClass('norequeridos');
+let boxDisable = document.getElementsByClassName('norequeridos');
 let toggleLink = document.getElementById('aviso');
+let textoAviso = document.getElementById('texto-aviso');
+let titulo     = document.getElementById('titulo');
+let btnSubmit  = document.getElementById('boton-submit');
 
 function toggleBox() {
-  if (boxDisable.style.display === 'none') {
-    boxDisable.style.display = 'block';
-    toggleLink.textContent = 'Inicio sesión';
-  } else {
-    boxDisable.style.display = 'none';
-    toggleLink.textContent = 'Registro';
-  }
+
+    let estaOculto = boxDisable[0].style.display === 'none';
+
+    for (let campo of boxDisable) {
+        if (estaOculto) {
+            campo.style.display = 'flex';
+        } else {
+            campo.style.display = 'none';
+        }
+    }
+
+    if (estaOculto) {
+        titulo.textContent        = '¡Preregistro abierto!';
+        btnSubmit.textContent     = '¡Regístrate!';
+        textoAviso.textContent    = '¿Ya tienes cuenta?';
+        toggleLink.textContent    = 'Inicia sesión';
+    } else {
+        titulo.textContent        = 'Inicia sesión';
+        btnSubmit.textContent     = '¡Inicie sesión!';
+        textoAviso.textContent    = '¿No tienes cuenta?';
+        toggleLink.textContent    = 'Regístrate';
+    }
 }
 
 toggleLink.addEventListener('click', toggleBox);
-
-
-
